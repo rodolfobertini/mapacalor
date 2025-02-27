10ed099c462445344b1d5c3ba43222bae74b28ca
rodolfobertini <rodolfobr@gmail.com>
2025-02-27 12:18:17 -0300

    hora no log de inicio

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config(); // Carregar variáveis de ambiente do arquivo .env
-const app = require('./app');
+import { listen } from './app';
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+listen(PORT, () => {
+    const dataHora = new Date().toISOString();
+    console.log(`[${dataHora}] Servidor rodando em http://localhost:${PORT}`);
 });
