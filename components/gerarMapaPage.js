834b834c8013bea18de84e486cf823b0ef100638
Rodolfo Bertini <rodolfobr@gmail.com>
2025-03-01 19:17:41 -0300

    menu

--- components/gerarMapaPage.js
+++ components/gerarMapaPage.js
@@ -4,25 +4,30 @@ const { gerarMapa } = require('./gerarMapa');
 const { gerarRodape } = require('./gerarRodape');
 
 function gerarMapaPage(lojaLat, lojaLon, quadrantes, ven_nrloja, gridSize, valorMinimo, startDate, endDate, menuPosition) {
+    let conteudo;
+    let styleExtra = '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"/><script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"></script>';
 
     if (menuPosition === 'top') {
-        const conteudo = `
-                    ${gerarMenu(ven_nrloja, gridSize, valorMinimo, startDate, endDate)}
-                    ${gerarMapa(lojaLat, lojaLon, quadrantes)}
+        conteudo = `
+            ${gerarMenu(ven_nrloja, gridSize, valorMinimo, startDate, endDate)}
+            ${gerarMapa(lojaLat, lojaLon, quadrantes)}
             ${gerarRodape()}
-    `;
-    const styleExtra = '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"/><script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"></script>';
-    return gerarHtmlBasico('Mapa de Calor', conteudo, styleExtra);
-    } 
-    else {
-        const conteudo = `
-                    ${gerarMenu(ven_nrloja, gridSize, valorMinimo, startDate, endDate)}
-                    ${gerarMapa(lojaLat, lojaLon, quadrantes)}
-                    ${gerarRodape()}
         `;
-    const styleExtra = '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"/><script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"></script>';
-    return gerarHtmlBasico('Mapa de Calor', conteudo, styleExtra);
+    } else {
+        conteudo = `
+            <div style="display: flex;">
+                <div style="width: 15%;">
+                    ${gerarMenu(ven_nrloja, gridSize, valorMinimo, startDate, endDate)}
+                </div>
+                <div style="width: 85%;">
+                    ${gerarMapa(lojaLat, lojaLon, quadrantes)}
+                </div>
+            </div>
+            ${gerarRodape()}
+        `;
     }
-};
+
+    return gerarHtmlBasico('Mapa de Calor', conteudo, styleExtra);
+}
 
 module.exports = { gerarMapaPage };
